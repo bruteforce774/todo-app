85e2e7c640aa18344d15a5827b1673cc6d001f12
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-03-24 21:37:59 +0100

    start on todo item component

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,7 @@
+import type { Todo } from './types'
+
+function TodoItem({ todo }: { todo: Todo }) {
+  // Render:
+  // - a checkbox, checked if todo.done is true
+  // - the todo text next to it
+}
